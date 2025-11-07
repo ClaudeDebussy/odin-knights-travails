69ed1da6289bf4ce7ccfe429a9e54eb7326434bb
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-11-07 09:39:00 +0500

    Start implementing class Square and knightMoves()

--- knights-travails.js
+++ knights-travails.js
@@ -1,43 +1,28 @@
-const validMoves = [
-  "upRight",
-  "rightUp",
-  "rightDown",
-  "downRight",
-  "downLeft",
-  "leftDown",
-  "leftUp",
-  "upLeft"
-]
+class Square {
+  constructor(position) {
+    position = this.position
+    moveNumber = 0;
+    previousSquare = null;
+    validMovesFromSquare;
 
-function knightMoves(position, destination, path = []) {
-  if (position === destination) {return}
-  validMoves.forEach(move => {
-    if (isValidMove(position, move)) {
-      position = (isValidMove(position, move))
-      console.log(`${position}`)
-      path.push(position)      
-      knightMoves(position, move)
-    }
-  });
-  return path
-}
-
-function isValidMove(position, move) {
-  let x = position[0]
-  let y = position[1]
-
-  if (x < 0 || x > 7 || y < 0 || y > 7) {
-    throw new Error("Invalid starting position.");
+    findValidMoves(position)
   }
-  
-  if (move === "upRight") { return (x + 1 <= 7) && (y + 2 <= 7) ? [x + 1, y + 2] : false}
-  if (move === "rightUp") { return (x + 2 <= 7) && (y + 1 <= 7) ? [x + 2, y + 1] : false}
-  if (move === "rightDown") { return (x + 2 <= 7) && (y - 1 >= 0) ? [x + 2, y - 1] : false}
-  if (move === "downRight") { return (x + 1 <= 7) && (y - 2 >= 0) ? [x + 1, y - 2 ] : false}
-  if (move === "downLeft") { return (x - 1 >= 0)  && (y - 2 >= 0) ? [x - 1, y - 2] : false}
-  if (move === "leftDown") { return (x - 2 >= 0) && (y - 1 >= 0) ? [x - 2, y -1] : false}
-  if (move === "leftUp") { return (x - 2 >= 0) && (y + 1 <= 7) ? [x - 2, y + 1] : false}
-  if (move === "upLeft") { return (x - 1 >= 0) && (y + 2 <= 7) ? [x - 1, y + 2] : false}
+
+  findValidMoves(position) {
+
+  }
 }
 
-console.log(knightMoves([0,0], [1,1]))
+
+function knightMoves(start, destination) {
+  let queue = []
+  let pointer = 0
+  let currentNode;
+
+  queue.push(start)
+  while (queue.length) {
+    currentNode = queue[pointer]    
+    // Add all valid moves from current node
+    pointer++
+  }
+}
