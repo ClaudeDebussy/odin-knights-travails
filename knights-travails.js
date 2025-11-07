556811ee5c5c77fb5e11d9350d5c347a0271adf0
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-11-07 10:49:14 +0500

    Implement findValidMoves() and begin class Knight

--- knights-travails.js
+++ knights-travails.js
@@ -1,28 +1,76 @@
 class Square {
-  constructor(position) {
-    position = this.position
-    moveNumber = 0;
-    previousSquare = null;
-    validMovesFromSquare;
-
-    findValidMoves(position)
+  constructor(position = [0,0]) {
+    this.position = position
+    this.moveNumber = 0
+    this.previousSquare = null
+    this.validMovesFromSquare = this.#findValidMoves(this.position)
   }
 
-  findValidMoves(position) {
+  #findValidMoves(position) {
+    const MOVES = [
+      "upRight",
+      "rightUp",
+      "rightDown",
+      "downRight",
+      "downLeft",
+      "leftDown",      
+      "leftUp",
+      "upLeft"
+    ]
 
+    const X = position[0]
+    const Y = position[1]
+
+    let validMoves = []
+
+    MOVES.forEach(move => {
+      if (move === "upRight") {(X + 1 <= 7) && (Y + 2 <= 7) ? validMoves.push(move) : null}
+      if (move === "rightUp") {(X + 2 <= 7) && (Y + 1 <= 7) ? validMoves.push(move) : null}
+      if (move === "rightDown") {(X + 2 <= 7) && (Y - 1 >= 0) ? validMoves.push(move) : null}
+      if (move === "downRight") {(X + 1 <= 7) && (Y - 2 >= 0) ? validMoves.push(move) : null}
+      if (move === "downLeft") {(X - 1 >= 0) && (Y - 2 >= 0) ? validMoves.push(move) : null}
+      if (move === "leftDown") {(X - 2 >= 0) && (Y - 1 >= 0) ? validMoves.push(move) : null}
+      if (move === "leftUp") {(X - 2 >= 0) && (Y + 1 <= 7) ? validMoves.push(move) : null}
+      if (move === "upLeft") {(X - 1 >= 0) && (Y + 2 <= 7) ? validMoves.push(move) : null}
+    })
+
+    return validMoves
+  }
+
+  printMoves(){
+    this.validMovesFromSquare.forEach(move => {
+      console.log(move)
+    })
   }
 }
 
+class Knight {
+  knightMoves(start, destination) {
+    if (
+      !start || 
+      Array.isArray(start) || 
+      start.length != 2 || 
+      start[0] > 7 || 
+      start[0] < 0 || 
+      start[1] > 7 || 
+      start[1] < 0
+    ) {
+        throw new Error("Invalid start data.");
+      }
 
-function knightMoves(start, destination) {
-  let queue = []
-  let pointer = 0
-  let currentNode;
+    let queue = []
+    let pointer = 0
+    let currentNode
+    
+    queue.push(start)
+    while (queue.length) {
+      currentNode = queue[pointer]    
+      // Add all valid moves from current node
 
-  queue.push(start)
-  while (queue.length) {
-    currentNode = queue[pointer]    
-    // Add all valid moves from current node
-    pointer++
+      pointer++
+    }
   }
 }
+
+let square = new Square(0);
+square.printMoves()
